f7540562a53968c5cf07e31c373cfc4a759e40c8
k-tsuda <h240503@gmail.com>
2022-05-03 22:40:40 +0900

    ex1-4

--- ch01/ex1-4.cpp
+++ ch01/ex1-4.cpp
@@ -2,8 +2,10 @@
 
 int main()
 {
+	// p. 35
 	// —ûK–â‘è 1.4 ‚Ì2
-	int a = 2;
+	// int a = 4 ‚¾‚ÆADafault‚ÉŠY“–‚·‚é‚ªADefault‚ªÁ‚¦‚½‚Ì‚Å‰½‚à•\¦‚³‚ê‚È‚¢
+	int a = 4;
 	switch (a + 1)
 	{
 		case 1:
@@ -17,4 +19,18 @@ int main()
 			std::cout << "a + 1 is 3" << std::endl;
 			break;
 	}
+
+	// —ûK–â‘è 1.4 ‚Ì3
+	/*
+	auto statement = a + 1;
+	if (statement == 1) {
+		std::cout << "a + 1 is 1" << std::endl;
+	} else if () (statement == 2) {
+		std::cout << "a + 1 is 2" << std::endl;
+	} else if () (statement == 1) {
+		std::cout << "a + 1 is 3" << std::endl;
+	} else {
+		std::cout << "a + 1 is not 1, 2, 3" << std::endl;
+	}
+	*/
 }
