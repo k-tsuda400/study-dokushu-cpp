1efa6aba72df9bff1ce3a5260e7c7fac9380b9e2
k-tsuda <h240503@gmail.com>
2022-05-03 23:37:02 +0900

    fix commentout

--- ch01/ex1-4.cpp
+++ ch01/ex1-4.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
 
-//int main()
-//{
-//	// p. 35
-//	// —ûK–â‘è 1.4 ‚Ì2
-//	// int a = 4 ‚¾‚ÆADafault‚ÉŠY“–‚·‚é‚ªADefault‚ªÁ‚¦‚½‚Ì‚Å‰½‚à•\¦‚³‚ê‚È‚¢
-//	int a = 4;
-//	switch (a + 1)
-//	{
-//		case 1:
-//			std::cout << "a + 1 is 1" << std::endl;
-//			break;
-//		case 2:
-//			std::cout << "a + 1 is 2" << std::endl;
-//			break;
-//		// default ‚ğÁ‚µ‚Ä‚İ‚é
-//		case 3:
-//			std::cout << "a + 1 is 3" << std::endl;
-//			break;
-//	}
-//
-//	// —ûK–â‘è 1.4 ‚Ì3
-//	/*
-//	auto statement = a + 1;
-//	if (statement == 1) {
-//		std::cout << "a + 1 is 1" << std::endl;
-//	} else if () (statement == 2) {
-//		std::cout << "a + 1 is 2" << std::endl;
-//	} else if () (statement == 1) {
-//		std::cout << "a + 1 is 3" << std::endl;
-//	} else {
-//		std::cout << "a + 1 is not 1, 2, 3" << std::endl;
-//	}
-//	*/
-//}
+int main()
+{
+	// p. 35
+	// —ûK–â‘è 1.4 ‚Ì2
+	// int a = 4 ‚¾‚ÆADafault‚ÉŠY“–‚·‚é‚ªADefault‚ªÁ‚¦‚½‚Ì‚Å‰½‚à•\¦‚³‚ê‚È‚¢
+	int a = 4;
+	switch (a + 1)
+	{
+		case 1:
+			std::cout << "a + 1 is 1" << std::endl;
+			break;
+		case 2:
+			std::cout << "a + 1 is 2" << std::endl;
+			break;
+		// default ‚ğÁ‚µ‚Ä‚İ‚é
+		case 3:
+			std::cout << "a + 1 is 3" << std::endl;
+			break;
+	}
+
+	// —ûK–â‘è 1.4 ‚Ì3
+	/*
+	auto statement = a + 1;
+	if (statement == 1) {
+		std::cout << "a + 1 is 1" << std::endl;
+	} else if () (statement == 2) {
+		std::cout << "a + 1 is 2" << std::endl;
+	} else if () (statement == 1) {
+		std::cout << "a + 1 is 3" << std::endl;
+	} else {
+		std::cout << "a + 1 is not 1, 2, 3" << std::endl;
+	}
+	*/
+}
--- ch01/ex1-7.cpp
+++ ch01/ex1-7.cpp
@@ -1,50 +1,48 @@
 #include <iostream>
 
-//int main()
-//{
-//	// p. 68
-//	///////////////////
-//	// 練習問題 1.7 (1)
-//	int array[] = { 4, 2, 1, 9, 5 };
-//
-//	// 配列の大きさを変数として獲得するイディオム。配列総数のバイト数 ÷ 配列の1要素のバイト数
-//	// 参考：https://qiita.com/SaitoAtsushi/items/ee17466c464fb7a270d2
-//	int sizeOfArray = sizeof(array) / sizeof(*array);
-//
-//	int i = 0;
-//	while (i < 5)
-//	{
-//		// before (配列を、頭から表示)
-//		//std::cout << array[i] << std::endl;
-//		//i += 1;
-//
-//		// after (配列を、後ろから表示)
-//		std::cout << array[sizeOfArray - i - 1] << std::endl;
-//		i += 1;
-//	}
-//
-//	// ---
-//	std::cout << "---" << std::endl;
-//
-//	///////////////////
-//	// 練習問題 1.7 (2)
-//	for (i = 0; i < 5; i++)
-//	{
-//		std::cout << array[sizeOfArray - i - 1] << std::endl;
-//	}
-//
-//	// ---
-//	std::cout << "---" << std::endl;
-//
-//	///////////////////
-//	// 練習問題 1.7 (3)
-//	i = 0;
-//	do 
-//	{
-//		std::cout << array[sizeOfArray - i - 1] << std::endl;
-//		i += 1;
-//	} while (i < 5);
-//	// 備考：whileのあとのセミコロンつけわすれ注意
-//
-//	
-//}
+int main()
+{
+	// p. 68
+	///////////////////
+	// 練習問題 1.7 (1)
+	int array[] = { 4, 2, 1, 9, 5 };
+
+	// 配列の大きさを変数として獲得するイディオム。配列総数のバイト数 ÷ 配列の1要素のバイト数
+	// 参考：https://qiita.com/SaitoAtsushi/items/ee17466c464fb7a270d2
+	int sizeOfArray = sizeof(array) / sizeof(*array);
+
+	int i = 0;
+	while (i < 5)
+	{
+		// before (配列を、頭から表示)
+		//std::cout << array[i] << std::endl;
+		//i += 1;
+
+		// after (配列を、後ろから表示)
+		std::cout << array[sizeOfArray - i - 1] << std::endl;
+		i += 1;
+	}
+
+	// ---
+	std::cout << "---" << std::endl;
+
+	///////////////////
+	// 練習問題 1.7 (2)
+	for (i = 0; i < 5; i++)
+	{
+		std::cout << array[sizeOfArray - i - 1] << std::endl;
+	}
+
+	// ---
+	std::cout << "---" << std::endl;
+
+	///////////////////
+	// 練習問題 1.7 (3)
+	i = 0;
+	do 
+	{
+		std::cout << array[sizeOfArray - i - 1] << std::endl;
+		i += 1;
+	} while (i < 5);
+	// 備考：whileのあとのセミコロンつけわすれ注意
+}
