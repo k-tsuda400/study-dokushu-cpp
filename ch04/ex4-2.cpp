06ea859e2faa6cf3729a19eebc9fb0c8846fea1a
k-tsuda <h240503@gmail.com>
2022-06-12 14:52:08 +0900

    4-2 (4-1 nai)

---
+++ ch04/ex4-2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+// p. 195
+///////////////////
+// 3Í‚ÌÍ–––â‘è
+
+// (1)
+int sum(int lhs, int rhs); // ‘O•ûéŒ¾
+
+int main()
+{
+    sum(10, 5);
+}
+
+int sum(int lhs, int rhs)
+{
+    return lhs + rhs;
+}
+
+// (2)
+class A
+{
+    int m_v;
+
+public:
+    explicit A(int v) : m_v(v)
+    {
+
+    }
+    int v() const
+    {
+        return m_v;
+    }
+};
